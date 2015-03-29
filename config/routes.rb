403acc1850328ee4f76aa73c964c8c2e14d747bc
beersheep <beersheep0109@gmail.com>
2015-03-29 23:22:10 +0800

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  
+
+
+
+
+  root 'pages#home' 
+
+
+  ###which means "pages" controller and "home" action. 
+  ###(can be found in 'pages_controller.rb', def home)
+
+
+
+
+
+
+
+#-----------
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
