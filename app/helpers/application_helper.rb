fded87fec1cc681dd4b6b4a8a15418c1cead1029
beersheep <beersheep0109@gmail.com>
2015-03-30 00:11:11 +0800

    added header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	# def current_user
+	# 	if current_user
+	# 		link_to "Setting", edit_user_registration_path
+	# 	else
+	# 		link_to "Login", new_user_session_path
+	# 	end
+	# end
 end
